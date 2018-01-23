ff3163c55ec500c0ab1fe102ff38c9eda5301025
fatih <fthylcnmt@gmail.com>
2018-01-23 20:20:13 +0300

    Initial

---
+++ src/transover/app/transover.java
@@ -0,0 +1,19 @@
+package transover.app;
+
+import java.awt.datatransfer.UnsupportedFlavorException;
+import java.io.IOException;
+
+public class transover {
+
+    public static void main (String[] args) throws UnsupportedFlavorException, InterruptedException, IOException {
+
+        gui arayüz;
+
+        arayüz = new gui();
+
+
+
+    }
+
+
+}
